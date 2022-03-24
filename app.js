e7546ce98bfd7247672b13e7b8ab12449e42d5d1
zusheng <imzusheng@163.com>
2022-03-24 16:22:39 +0800

    update: 简化

--- app.js
+++ app.js
@@ -1,35 +1,14 @@
-const koaBody = require('koa-body')
 const KoaStatic = require('koa-static')
 const cors = require('koa2-cors')
 const Koa = require('koa')
 const app = new Koa()
-const router = require('./src/router')
 
-const serverPort = 3000
-const serverUrl = `http://localhost:${3000}`
-const {exec} = require("child_process")
+const serverPort = 3301
+const serverUrl = `http://localhost:${3301}`
 const path = require("path")
 
 
 app
-    .use(koaBody())
     .use(cors())
     .use(KoaStatic(path.join(path.resolve(), 'src')))
-    .use(router.routes())
-    .use(router.allowedMethods())
-    .listen(serverPort, () => {
-        console.log(`server running at ${serverUrl}`)
-        return
-        // 启动后打开浏览器
-        switch (process.platform) {
-            case "darwin": //mac系统使用 一下命令打开url在浏览器
-                exec(`open ${serverUrl}`)
-                break
-            case "win32": //win系统使用 一下命令打开url在浏览器
-                exec(`start ${serverUrl}`)
-                break
-            // 默认mac系统
-            default:
-                exec(`open ${serverUrl}`)
-        }
-    })
+    .listen(serverPort, () => console.log(`server running at ${serverUrl}`))
--- src/router/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('@koa/router')()// koa 路由中间件
-const commonRouter = require('./common')
-const proxyRouter = require('./proxy')
-
-router.use('/proxy', proxyRouter.routes())
-router.use('/api', commonRouter.routes())
-
-module.exports = router
