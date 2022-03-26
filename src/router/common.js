725f24761e2a69a64b93c66b2a5320e62219d3aa
zusheng <imzusheng@163.com>
2022-03-26 12:29:30 +0800

    update: 提取弹幕、封面

--- src/router/common.js
+++ src/router/common.js
@@ -1,7 +1,9 @@
 const axios = require("axios");
 const request = require("request")
+const https = require("https");
 const router = require('@koa/router')()
-const passThrough = require('stream').PassThrough;
+const passThrough = require('stream').PassThrough
+const {PromiseQueue} = require('../util')
 
 router.get('/search', async ctx => {
   const {url} = ctx.query
@@ -76,6 +78,62 @@ router.get('/download', async ctx => {
   ctx.body = bodyPipe
   // TODO 浏览器取消保存仍在下载
   // ctx.body = fs.createReadStream(path.join(path.resolve(), 'download/1647947629838.mp4')).pipe(passThrough());
+})
+
+router.get('/dm', async ctx => {
+  const {
+    avid,
+    cid
+  } = ctx.query
+  
+  function getSegmentMax() { // 获取分段数， 六分钟分一段
+    return new Promise(resolve => {
+      axios(`https://api.bilibili.com/pgc/player/web/playurl?avid=${avid}&cid=${cid}`).then(res => {
+        resolve(Math.ceil(res.data.result.timelength / 1000 / 60 / 6))
+      })
+    })
+  }
+  
+  const segment_max = await getSegmentMax()
+  const taskList = new Array(segment_max).fill('').map((v, i) => {
+    return {
+      cb: () => get(i + 1),
+      index: i + 1
+    }
+  })
+  const promiseQueue = new PromiseQueue(taskList, 100)
+  
+  
+  function get(segment_index) {
+    return new Promise(resolve => {
+      https.get(`https://api.bilibili.com/x/v2/dm/web/seg.so?type=1&oid=${cid}&segment_index=${segment_index}`, response => {
+        let chunks = Buffer.alloc(0)
+        response.on('data', data => chunks = Buffer.concat([chunks, Buffer.from(data)]))
+        response.on('end', () => {
+          try {
+            const message = lp.decode(chunks)
+            const objects = lp.toObject(message, {
+              bool: Boolean,
+              longs: Number,
+              enums: Number,
+              bytes: String,
+              Object: String
+            })
+            resolve(objects.elems)
+          } catch (e) {
+            resolve(null)
+          }
+        })
+      })
+    })
+  }
+  
+  ctx.set({
+    'Content-Type': 'application/octet-stream',
+    'Content-disposition': `attachment; filename=dm_${Date.now()}.json`
+  })
+  
+  ctx.body = await promiseQueue.getResult()
 })
 
 module.exports = router
--- src/util/index.js
+++ src/util/index.js
@@ -8,7 +8,55 @@ const writeDownload = async (filename) => {
     writeStream.once('ready', () => resolve(writeStream))
   })
 }
+// 创建Promise队列
+const PromiseQueue = class PromiseQueue {
+  /**
+   * 传入参数
+   * @param taskList
+   * @param time 请求间隔时间
+   */
+  constructor(taskList, time) {
+    this.result = {}
+    this.time = time
+    this.promiseList = taskList.map(task => this.createPromise(task.cb, task.index))
+    console.log(this.promiseList)
+  }
+  
+  // 开始执行队列，获取结果
+  /**
+   * @return {Promise<Object>}
+   */
+  getResult = () => {
+    // 开始执行任务
+    this.promiseEnd = this.promiseList.reduce((prev, next) => {
+      return prev.then(() => next())
+    }, Promise.resolve())
+    // 执行完成获取结果
+    return new Promise(resolve => {
+      this.promiseEnd.then(() => resolve(this.result))
+    })
+  }
+  // 创建任务
+  createPromise = (cb, index) => {
+    return () => {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          cb()
+              .then(res => {
+                this.result[index] = res
+                resolve()
+              })
+              .catch(res => {
+                this.result[index] = res
+                resolve()
+              })
+        }, this.time)
+      })
+    }
+  }
+}
 
 module.exports = {
-  writeDownload
+  writeDownload,
+  PromiseQueue
 }
